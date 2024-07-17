e42e7167fa316b72ea7695d0ee8687dc248aeeec
Shashwat Shekhar <119045090+shashwat22473@users.noreply.github.com>
2024-07-17 11:24:39 +0530

    Create MaximumLengthOfRepeatedSubarray.cpp

---
+++ MaximumLengthOfRepeatedSubarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+        int ans=0;
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if(nums1[i]==nums2[j]){dp[i+1][j+1]=dp[i][j]+1;ans=max(ans,dp[i+1][j+1]);}
+            }
+        }
+        return ans;
+    }
+};
